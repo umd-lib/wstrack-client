ecc7fe94194a426da588e5aa75fe62630bfcf5a5
Ben Wallberg <wallberg@umd.edu>
2012-09-23 06:28:33 -0400

    improve documentation

--- script/wstrack-client.sh
+++ script/wstrack-client.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# command line arguments
+# command line argument 1
+# status (required) - login or logout
 STATUS="$1"
 if [ -z "$STATUS" ]; then
   echo "Status is required (login or logout)"
   exit 1
 fi
 
+# command line argument 2
+# environment (optional) - server to contact (prod, stage, dev, or local) 
 ENVIRONMENT=local
 if [ -n "$2" ]; then
   ENVIRONMENT="$2"
 fi
 
+# command line argument 3
+# debug (optional) - display debugging information
 DEBUG="false"
 if [ -n "$3" ]; then
   DEBUG="true"
@@ -23,6 +28,7 @@ IP=$(/sbin/ifconfig | /usr/bin/grep 'inet ' | /usr/bin/grep -v 127\.0\.0\.1 | /u
 OSNAME=$(/usr/bin/sw_vers -productName)
 OSVER=$(/usr/bin/sw_vers -productVersion)
 
+# send information to workstation tracking server
 java \
   -Dwstrack.debug="$DEBUG" \
   -Dwstrack.env="$ENVIRONMENT" \
