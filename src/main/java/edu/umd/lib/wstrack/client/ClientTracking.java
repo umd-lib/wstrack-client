49493f7faf58d5482b7ea741676348488e910d7e
Ben Wallberg <wallberg@umd.edu>
2012-12-31 09:02:53 -0500

    make the track submission logic reusable
    separate cli interace in main() from submission in track()

--- src/main/java/edu/umd/lib/wstrack/client/ClientTracking.java
+++ src/main/java/edu/umd/lib/wstrack/client/ClientTracking.java
@@ -44,72 +44,83 @@ public class ClientTracking {
 
     try {
 
-      // map environment to baseUrl
       String env = System.getProperty("wstrack.env", "local");
-      String baseUrl = null;
-
-      if (env.equals("prod")) {
-        baseUrl = "https://www.lib.umd.edu/wstrack/track";
-
-      } else if (env.equals("stage")) {
-        baseUrl = "https://wwwstage.lib.umd.edu/wstrack/track";
-
-      } else if (env.equals("dev")) {
-        baseUrl = "https://wwwdev.lib.umd.edu/wstrack/track";
-
-      } else {
-        baseUrl = "http://localhost:8080/wstrack-server/track";
-      }
-      log.debug("base url: " + baseUrl);
 
       // gather params
       String username = System.getProperty("wstrack.username");
       if (username == null) {
         throw new Exception("wstrack.username property is required");
       }
-      log.debug("username: " + username);
 
       String computerName = System.getProperty("wstrack.computerName");
       if (computerName == null) {
         throw new Exception("wstrack.computerName property is required");
       }
-      log.debug("computerName: " + computerName);
 
       String status = System.getProperty("wstrack.status");
       if (status == null) {
         throw new Exception("wstrack.status property is required");
       }
-      log.debug("status: " + status);
 
       String os = System.getProperty("wstrack.os");
       if (os == null) {
         throw new Exception("wstrack.os property is required");
       }
-      log.debug("os: " + os);
 
-      // build tracking url
-      StringBuffer sb = new StringBuffer(baseUrl);
-      sb.append("/" + URLEncoder.encode(computerName, "UTF-8"));
-      sb.append("/" + status);
-      sb.append("/" + URLEncoder.encode(os, "UTF-8"));
-      sb.append("/" + URLEncoder.encode(username, "UTF-8"));
+      track(env, username, computerName, status, os);
 
-      // open the connection
-      URL url = new URL(sb.toString());
-      URLConnection conn = url.openConnection();
-
-      // Get the response
-      BufferedReader rd = new BufferedReader(new InputStreamReader(
-          conn.getInputStream()));
-      String line;
-      while ((line = rd.readLine()) != null) {
-        log.debug("response: " + line);
-      }
-      // wr.close();
-      rd.close();
     } catch (Exception e) {
       log.error("Error in ClientTracking", e);
     }
-
   }
+
+  /*
+   * @Javadoc - Submit track event
+   */
+  public static void track(String env, String username, String computerName,
+      String status, String os) throws MalformedURLException, IOException {
+
+    // map environment to baseUrl
+    String baseUrl = null;
+
+    if (env.equals("prod")) {
+      baseUrl = "https://www.lib.umd.edu/wstrack/track";
+
+    } else if (env.equals("stage")) {
+      baseUrl = "https://wwwstage.lib.umd.edu/wstrack/track";
+
+    } else if (env.equals("dev")) {
+      baseUrl = "https://wwwdev.lib.umd.edu/wstrack/track";
+
+    } else {
+      baseUrl = "http://localhost:8080/wstrack-server/track";
+    }
+    log.debug("base url: " + baseUrl);
+    log.debug("username: " + username);
+    log.debug("computerName: " + computerName);
+    log.debug("status: " + status);
+    log.debug("os: " + os);
+
+    // build tracking url
+    StringBuffer sb = new StringBuffer(baseUrl);
+    sb.append("/" + URLEncoder.encode(computerName, "UTF-8"));
+    sb.append("/" + status);
+    sb.append("/" + URLEncoder.encode(os, "UTF-8"));
+    sb.append("/" + URLEncoder.encode(username, "UTF-8"));
+
+    // open the connection
+    URL url = new URL(sb.toString());
+    URLConnection conn = url.openConnection();
+
+    // Get the response
+    BufferedReader rd = new BufferedReader(new InputStreamReader(
+        conn.getInputStream()));
+    String line;
+    while ((line = rd.readLine()) != null) {
+      log.debug("response: " + line);
+    }
+
+    rd.close();
+  }
+
 }
